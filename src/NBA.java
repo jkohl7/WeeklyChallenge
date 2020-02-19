36de20993c7beedb6a0ffd6ee979954f2a231ea2
jkohl7 <jkohl7@my.wctc.edu>
2020-02-19 15:56:58 -0600

    Update

--- src/NBA.java
+++ src/NBA.java
@@ -2,22 +2,38 @@ import java.util.Scanner;
 
 public class NBA {
         //Central Division
-        public enum Teams {Bulls("Bul"), Cavaliers("Cav"), Pistons("Pis"), Pacers("Pac"), Bucks("Buc");
+        public enum Teams {Bulls("Bul", "Chicago", "Bulls"), Cavaliers("Cav", "Cleveland", "Cavaliers"), Pistons("Pis", "Detroit", "Pistons"), Pacers("Pac", "Indiana", "Pacers"), Bucks("Buc", "milwaukee", "Bucks");
 
             private String abbrev;
+            private String city;
+            private String name;
 
-            private Teams(String abbrev) {
+
+            Teams(String abbrev, String city, String name){
                 this.abbrev = abbrev;
+                this.city = city;
+                this.name = name;
             }
 
-            public String getAbbreviations() {
+            public String getAbbrev() {
                 return abbrev;
             }
-        }
+
+            public String getCity() {
+                return city;
+            }
+
+            public String getName() {
+                return name;
+            }
+        }M
 
     public static void main(String[] args) {
         System.out.println("What is your favorite team in the Central Division?");
         Scanner keyboard = new Scanner(System.in);
+        String favorite = keyboard.nextLine();
+
+        for(Teams)
 
     }
 }
