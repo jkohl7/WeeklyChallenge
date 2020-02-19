a763e1f364ffc81b5e781e890c63a47c60ac9ffb
jkohl7 <jkohl7@my.wctc.edu>
2020-02-19 13:22:53 -0600

    Completed Number

--- src/Number.java
+++ src/Number.java
@@ -4,8 +4,25 @@ public class Number {
     public static void main(String[] args) {
         System.out.println("Enter a Number");
         Scanner keyboard = new Scanner(System.in);
-        int num = keyboard.nextInt();
+        double num = keyboard.nextDouble();
 
-        System.out.println();
+        if (num <= Byte.MAX_VALUE){
+            System.out.println("Byte");
+        }else if(num <= Short.MAX_VALUE){
+            System.out.println("Short");
+        }else if(num <= Integer.MAX_VALUE) {
+            System.out.println("Int");
+        }else if(num <= Long.MAX_VALUE) {
+            System.out.println("Long");
+        }else if(num <= Float.MAX_VALUE) {
+            System.out.println("Float");
+        }else if(num <= Double.MAX_VALUE) {
+            System.out.println("Double");
+        }
+
+
+
+
+
     }
 }
